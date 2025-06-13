37b19846034d3e2bdcb3bfdfde2a2e6d763851a0
roguepod-litecasst <roguepodlitecast@gmail.com>
2025-06-13 11:24:55 -0700

    add SEO optimizations

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ const App: React.FC = () => {
     },
     {
       href: "https://podcasts.apple.com/gb/podcast/roguepod-litecast/id1774367401",
-      text: "Apple",
+      text: "Apple Podcasts",
       icon: (
         <svg className="w-full h-full" viewBox="0 0 24 24" fill="currentColor">
           <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/>
@@ -213,7 +213,7 @@ const App: React.FC = () => {
 
             {/* Description */}
             <p className="text-white text-opacity-90 text-base sm:text-lg mb-6 sm:mb-8 font-normal leading-relaxed max-w-full mx-auto animate-slideInRight">
-              RoguePod LiteCast is a roguelite review podcast released every other Wednesday. Each episode Danny and David discuss a different roguelite and add it to the ultimate roguelite tierlist.
+              RoguePod LiteCast is a roguelite / action roguelike review podcast released every other Wednesday. Each episode Danny and David discuss a different roguelite and add it to the ultimate roguelite tierlist.
             </p>
 
             {/* Tier List Button */}
